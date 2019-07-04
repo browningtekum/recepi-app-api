3d78b5ca5f08e9a74d7d1f8bddc94adf379e49f4
Browning Meyah Tekum <brown.ing@hotmail.com>
2019-07-04 17:32:54 +0200

    Started recipe app

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@hot.com', password='password1234'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     
@@ -33,7 +40,15 @@ class ModelTests(TestCase):
             'test@hot.com',
             'test123'
         )
-        
-        return user
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def tes_tag_str(self):
+        """Test the taf string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
